a481d3fc3fd67ca2391454cddbf94a2910864e5a
shahid <shahidgithub9390@gmail.com>
2024-03-29 11:00:23 +0000

    code

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className='container'>
+    <div>
+      <h1>Congratulations</h1>
+    </div>
+    <div className='personCard'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+        className=''
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
